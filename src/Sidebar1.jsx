8a8df2b0406dfe4f84b5af4419a889d3a2cd1f8c
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-05-29 17:46:56 +0530

    removed showText state

--- src/Sidebar1.jsx
+++ src/Sidebar1.jsx
@@ -2,54 +2,38 @@ import { useEffect, useState } from "react";
 
 export default function Sidebar1() {
     const [open,setOpen]=useState(true);
-    const [showText,setShowText]=useState(true);
-    let timer=0;
-    useEffect(()=>{
-      if(open){
-        timer=setTimeout(()=>{
-          setShowText(true);
-          console.log("open: "+open+" showText: "+showText);
-        },700   );
-      }
-      else{
-        setShowText(false);
-        console.log("open: "+open+" showText: "+showText);
-      }
-        return (()=>{
-            clearTimeout(timer);  
-        })
-    },[open])
+    
   return (
     <div className=" w-full flex h-screen">
       <div className={`transition-all duration-500 ease-in-out delay-200 ${open? "lg:w-1/5 w-[45px]":"w-[45px]"}`}>
         <div className="flex bg-red-300 justify-between flex-row-reverse p-1" onClick={()=>{setOpen(x=>!x);}} >
             <img className="w-[40px] " src="../../sidebar-flip-svgrepo-com.svg" alt="" />
-            {showText&&<div className={"w-full p-2 transition-all lg:delay-700 lg:opacity-100 lg:scale-100 opacity-0 scale-0 max-h-[48px]"} >
+            {<div className={`w-full p-2 transition-all  ${open?"lg:opacity-100 lg:scale-100 lg:delay-700":""} opacity-0 scale-0 max-h-[48px]`} >
             {/* {showText&&<div className=" w-full p-2 lg:block hidden" > */}
                 Toggle Sidebar
             </div>}
 
         </div>
-        <SidebarOption showText={showText}/>
-        <SidebarOption showText={showText}/>
-        <SidebarOption showText={showText}/>
-        <SidebarOption showText={showText}/>
-        <SidebarOption showText={showText}/>
+        <SidebarOption />
+        <SidebarOption />
+        <SidebarOption />
+        <SidebarOption />
+        <SidebarOption />
       </div>
       <div className="w-full bg-blue-300">hi there</div>
     </div>
   );
 }
 
-function SidebarOption({showText}) {
+function SidebarOption() {
   return (
     <div className="flex flex-row-reverse items-center w-full justify-between ${open?m-2:m-2}">
-      <img
+      {/* <img
         className="w-[35px] h-[35px] m-1 p-1"
         src="../../home-svgrepo-com.svg"
         alt="Home"
         />
-        {showText?<div className="p-2 lg:block hidden">Home</div>:null}
+        {showText?<div className="p-2 lg:block hidden">Home</div>:null} */}
     </div>
   );
 }
