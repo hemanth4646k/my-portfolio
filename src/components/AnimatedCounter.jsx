aa7b5e40c177ee99a195bc28a1925ebceecbb558
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-06-26 17:13:44 +0530

    completed hero section and implemented scroll down using button

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { counterItems } from '../constants'
+import CountUp from 'react-countup'
 const AnimatedCounter = () => {
   return (
-    <div id="counter">
-      hello hello
+    <div id="counter" className='padding-x-lg xl:mt-0 mt-32'>
+      <div className='mx-auto grid-4-cols'>
+        {counterItems.map((item)=>(
+            <div className='bg-zinc-800 rounded-lg p-10 flex-col justify-center'>
+                <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
+                    <CountUp end={item.value} suffix={item.suffix}></CountUp> 
+                </div>
+                <div className='text-white-50 text-lg '>{item.label}</div>
+            </div>
+        ))}
+
+      </div>
     </div>
   )
 }
