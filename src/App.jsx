7ec3ef2fc41b3fb6d7ae3d8feaf55659207b4b79
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-07-14 22:25:50 +0530

    made 3d model rendering smooother and made sure it did not interrupt the animation of starting animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Hero } from "./sections/Hero";
 import Showcase from "./sections/Showcase";
 import OceanScene from "./sections/HeroModel/OceanScene";
 import { PanProvider, usePan } from "./context/PanProvider";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -15,25 +16,35 @@ function App() {
 
 function AppComponent() {
   const { handlePointerDown, handlePointerMove, handlePointerUp } = usePan();
+  const [startLoading, setStartLoading] = useState(false);
+  useEffect(() => {
+    // 200ms delay before starting 3D loading
+    const timer = setTimeout(() => {
+      setStartLoading(true);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div>
-      <div className="hero-3d-layout">
-        <Canvas
-          camera={{ fov: 55, near: 1, far: 20000 }}
-          style={{
-            pointerEvents: "auto",
-            height: "100vh",
-            width: "100vw",
-            background: "#000",
-            zIndex: "0",
-          }}
-          onPointerDown={handlePointerDown}
-          onPointerUp={handlePointerUp}
-          onPointerMove={handlePointerMove}
-        >
-          <OceanScene />
-        </Canvas>
-      </div>
+      { (
+        <div className={`hero-3d-layout transition-opacity duration-500 ${startLoading ? "opacity-100" : "opacity-0"}`}>
+          <Canvas
+            camera={{ fov: 55, near: 1, far: 20000 }}
+            style={{
+              pointerEvents: "auto",
+              height: "100vh",
+              width: "100vw",
+              background: "#000",
+              zIndex: "0",
+            }}
+            onPointerDown={handlePointerDown}
+            onPointerUp={handlePointerUp}
+            onPointerMove={handlePointerMove}
+          >
+            <OceanScene />
+          </Canvas>
+        </div>
+      )}
       <div className="relative select-none" style={{ pointerEvents: "none" }}>
         <Hero></Hero>
         <Showcase></Showcase>
